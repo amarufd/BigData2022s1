a38a0c85e2fb32a13fe85d5a4af3bbcdcd7042d3
Amaru Fernández Durán <amarud@gmail.com>
2022-04-29 21:08:09 -0400

    se crea mi primer script

---
+++ mi_primer_script.R
@@ -0,0 +1,28 @@
+# Suma
+4 + 4
+
+# Multiplicacion
+3 * 5
+
+# Division
+4 / 6
+
+# Elevado
+2 ^ 2
+3**2
+
+# Raiz
+4**1/2
+
+# se guarda en respuesta la operacion
+# 4 + 4
+respuesta <- 4 + 4
+
+# Muestra la innformacion que uno decide
+print(respuesta)
+
+
+# almacenando texto
+respuesta <- "Que tengan un bonito descanso"
+print(respuesta)
+
